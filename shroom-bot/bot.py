67ea2a469ee131292828a54cfe63632eab5059b7
BoySanic <boysonicrevived@gmail.com>
2025-12-09 16:03:27 -0800

    Fix issue with index again

--- shroom-bot/bot.py
+++ shroom-bot/bot.py
@@ -45,7 +45,7 @@ async def leaderboard(
     if(response.status_code == 200):
         json_response = json.loads(response.content)
         for x in range(1, limit):
-            message += f"{x}: <@{json_response[x]['discord_id']}>, {json_response[x]['seed']}, {json_response[x]['claimed_size']}, {json_response[x]['calculated_size']}, {json_response[x]['result_id']}\n"
+            message += f"{x}: <@{json_response[str(x)]['discord_id']}>, {json_response[str(x)]['seed']}, {json_response[str(x)]['claimed_size']}, {json_response[str(x)]['calculated_size']}, {json_response[str(x)]['result_id']}\n"
         await interaction.response.send_message(message)
     else:
         await interaction.response.send_message("Zoinks scoob! That one didn't work.")
